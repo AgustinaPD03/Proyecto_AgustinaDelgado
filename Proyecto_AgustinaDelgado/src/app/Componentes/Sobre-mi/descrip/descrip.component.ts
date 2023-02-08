5b4e8c9aeec086c25120a46d9a8cbd391a39010c
AgustinaPD03 <kagusdelgado@gmail.com>
2023-02-08 12:19:54 -0300

    08/02/2023
    Creación de los componentes basicos para mantener un orden, no se creo nada dentro de cada componente

---
+++ Proyecto_AgustinaDelgado/src/app/Componentes/Sobre-mi/descrip/descrip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descrip',
+  templateUrl: './descrip.component.html',
+  styleUrls: ['./descrip.component.css']
+})
+export class DescripComponent {
+
+}
--- Proyecto_AgustinaDelgado/src/app/app.module.ts
+++ Proyecto_AgustinaDelgado/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { BannerComponent } from './Componentes/banner/banner.component';
+import { DescripComponent } from './Componentes/Sobre-mi/descrip/descrip.component';
+import { EstudiosComponent } from './Componentes/Sobre-mi/estudios/estudios.component';
+import { ExpComponent } from './Componentes/Sobre-mi/exp/exp.component';
+import { HabilidadesComponent } from './Componentes/Sobre-mi/habilidades/habilidades.component';
+import { ProyectosComponent } from './Componentes/proyectos/proyectos.component';
+import { FooterComponent } from './Componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    DescripComponent,
+    EstudiosComponent,
+    ExpComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
